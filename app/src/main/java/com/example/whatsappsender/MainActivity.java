bfb0ae686f50e67cd1d402f4e52be84698d7f868
urigott <urigott@gmail.com>
2024-07-21 01:18:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/whatsappsender/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.whatsappsender;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextInput;
+    private Button buttonSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.editTextInput);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editTextInput.getText().toString();
+                String url = "https://wa.me/" + inputText;
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
